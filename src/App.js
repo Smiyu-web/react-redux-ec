db64593e3aec5b14128366ea2ecbfb6a08fe55e4
Miu <miu@Mius-MBP.hitronhub.home>
2021-03-15 22:07:40 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
-import { signInAction } from "./reducks/users/actions";
+import React from "react";
+import RouterFile from "./RouterFile";
 
-function App() {
-  const dispatch = useDispatch();
-  const selector = useSelector((state) => state);
-  console.log(selector.users);
-
+const App = () => {
   return (
-    <div className="App">
-      <button
-        onClick={() => dispatch(signInAction({ uid: "0001", username: "miu" }))}
-      >
-        Sign In
-      </button>
-    </div>
+    <main>
+      <RouterFile />
+    </main>
   );
-}
+};
 
 export default App;
